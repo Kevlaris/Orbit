107b00e8d22398542c2de1a7e05764b105d43b20
Kevlaris <team3csh@gmail.com>
2022-09-27 20:24:16 +0200

    Particle-based star visual

--- Assets/Scripts/Stars/Star.cs
+++ Assets/Scripts/Stars/Star.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Star : MonoBehaviour
+public class Star : CelestialBody
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[Header("Star Attributes")]
+    public Color chromaticity;
+	public float solarRadius;
+	public float luminosity;
+	public float temperature;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	Material mat;
+	StellarClassification.StellarClass stellarClass;
+
+	private void Reset()
+	{
+		base.radius.Unit = Length.Unit.km;
+		base.radius.isStatic = true;
+	}
+
+	private void OnValidate()
+	{
+		base.radius.Unit = Length.Unit.km;
+		base.radius.isStatic = true;
+		base.radius.amount = solarRadius * Universe.solarRadius.amount;
+		mass = Mathf.Pow(Universe.lengthScale, 3) * (surfaceGravity * Mathf.Pow(Length.Convert(radius, Length.Unit.m).amount, 2) / Universe.gravitationalConstant);
+		meshHolder = transform.GetChild(0);
+		meshHolder.localScale = Vector3.one * Length.ConvertToWorld(radius) * 2;
+	}
+
+	private void Awake()
+	{
+		velocity = initialVelocity;
+		rb = GetComponent<Rigidbody>();
+		rb.mass = mass;
+		rb.useGravity = false;
+	}
+
+	private void Start()
+	{
+		mat = meshHolder.GetComponent<MeshRenderer>().material;
+		mat.EnableKeyword("_EMISSION");
+		stellarClass = StellarClassification.Classify(temperature, luminosity * Universe.solarLuminosity);
+		chromaticity = stellarClass.SpectralClass.chromaticity;
+	}
+
+	private void Update()
+	{
+		mat.SetColor("_EmissionColor", chromaticity * Mathf.LinearToGammaSpace(luminosity * .1f));
+	}
 }
--- Assets/Scripts/Universe.cs
+++ Assets/Scripts/Universe.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public static class Universe
 {
-    public const float gravitationalConstant = 1f;
+    public const float gravitationalConstant = 1;
     public const float physicsTimeStep = 0.01f;
     public const float lengthScale = 0.0001f;
     public static float solarLuminosity = 3.8f * Mathf.Pow(10, 26);
+    /// <summary>
+    /// Equatorial radius of the Sun. (in km)
+    /// </summary>
+    public static Quantity solarRadius = new Quantity(695700, Length.Unit.km, true);
 }
